8ce743e4e456d9244aaa5f2c90ec317d25a72510
vlavash <39505044+vlavash@users.noreply.github.com>
2018-05-22 20:20:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+/*By: Valerie Lavash
+Description: This is a project for an educational course through 
+Albany Can Code which was assigned by Steve Cifarelli on 5/1/2018. 
+  Our task was to use the OWL Carousel pluggin and do something 
+  creative with it.    */
+
+
+            $(function(){
+                $('.owl-carousel').owlCarousel({
+                    loop:true,
+                    margin:10,
+                    autoplay: true,
+                    autoplayTimeout: 5000,
+                    nav:true,
+                    mouseDrag:false,
+                    items:3,
+                    
+                });
+            });
+
+           
+      
+
+        
